5df9b3f1b93daa342acef0dcf739586592cb4587
lasama <38429883+lasama@users.noreply.github.com>
2021-02-18 13:37:33 +0200

    Update converter.js

--- converter.js
+++ converter.js
@@ -20,16 +20,16 @@ svgTPistol
 svgTStatic
 */
 //variables for .svg files
-var svgSMoving = document.getElementById('ScopeMoving.svg');
-var svgSPistol = document.getElementById('ScopePistol.svg');
-var svgSStatic = document.getElementById('ScopeStatic.svg');
-var svgShooting = document.getElementById('Shooting.svg');
-var svgTMoving = document.getElementById('TargetMoving.svg');
-var svgTPistol = document.getElementById('TargetPistol.svg');
-var svgTStatic = document.getElementById('TargetStatic.svg');
+//var svgSMoving = document.getElementById('ScopeMoving');
+var svgSPistol = document.getElementById('ScopePistol');
+var svgSStatic = document.getElementById('ScopeStatic');
+var svgShooting = document.getElementById('Shooting');
+var svgTMoving = document.getElementById('TargetMoving');
+var svgTPistol = document.getElementById('TargetPistol');
+var svgTStatic = document.getElementById('TargetStatic');
 
 //svg files sizes
-let {width, height} = svgSMoving.getBBox();
+//let {width, height} = svgSMoving.getBBox();
 let {width, height} = svgSPistol.getBBox();
 let {width, height} = svgSStatic.getBBox();
 let {width, height} = svgShooting.getBBox();
@@ -38,10 +38,23 @@ let {width, height} = svgTPistol.getBBox();
 let {width, height} = svgTStatic.getBBox();
 
 //clone to canvas
-let clonedsvgSMoving = svgSMoving.cloneNode(true);
+//let clonedsvgSMoving = svgSMoving.cloneNode(true);
 let clonedsvgSPistol = svgSPistol.cloneNode(true);
 let clonedsvgSStatic = svgSStatic.cloneNode(true);
 let clonedsvgShooting = svgShooting.cloneNode(true);
 let clonedsvgTMoving = svgTMoving.cloneNode(true);
 let clonedsvgTPistol = svgTPistol.cloneNode(true);
 let clonedsvgTStatic = svgTStatic.cloneNode(true);
+// true for deep clone(check what does it mean)
+
+let outerHTMLSP = clonedsvgSPistol.outerHTMLSP,
+blobSP = new Blob([outerHTML],
+    {type:'image/svg+xml;charset=utf-8'}); //ScopePistol
+/*let 
+    blobSS = new Blob([outerHTML],
+    {type:'image/svg+xml;charset=utf-8'}); //ScopeStatic
+blobShooting = new Blob([outerHTML],
+    {type:'image/svg+xml;charset=utf-8'}); //Shooting
+blobTM = new Blob([outerHTML],
+    {type:'image/svg+xml;charset=utf-8'}); //TargetMoving
+*/
